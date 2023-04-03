4fe21b619b238ebc5229a46433de40a1f0cf7197
wren-pnt <c.spencerstock@gmail.com>
2023-04-03 12:30:10 -0600

    03-04-2023
    adding email link

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
+
+  $to = "c.spencerstock@gmail.com";
+  $headers = "From: $email\r\nReply-To: $email\r\n";
+
+  mail($to, $subject, $message, $headers);
+
+  echo "Thank you for your message!";
+}
+?>
